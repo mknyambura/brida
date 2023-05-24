fdb3a6dfab0030dc3c00d7ddb9971964b08ec985
mknyambura <kariuki.mercie@gmail.com>
2023-05-24 20:57:10 +0300

    update footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,17 +17,6 @@ function Header() {
                     </Link>
                 </div>
                 <div className='flex flex-row justify-between gap-8'>
-                    {/* <div className='text-white relative' onMouseLeave={() => setOpen(false)}>
-                        <h1 onMouseOver={() => setOpen(true)} className='flex  flex-row  items-center gap-2 hover:text-[#ff9b00]'>Our Programs <SlArrowDown/></h1>
-                        <ul className={`absolute right-0 w-40 py-2 mt-2 rounded-lg shadow-xl ${
-                            open ? "block" : "hidden"
-                            }`}
-                        >
-                            <li><a href="">Undergraduate</a></li>
-                            <li><a href="">Postgraduate</a></li>
-                            <li><a href="">Certifications</a></li>
-                        </ul>
-                    </div> */}
                     <Link to='/programs'className='hover:text-[#ff9b00]'>Our Programs</Link>
                     <h1 className='hover:text-[#ff9b00]'><a href='#awards'>Awards</a></h1>
                     <Link to='/about-us' className='hover:text-[#ff9b00]'>About Us</Link>
